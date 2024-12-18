41353d7dd9eff141c8acb576a70f1d125ad7fe01
Jakub Kasem <jakub.kasem@nxp.com>
2024-12-18 15:20:15 +0100

    Main - argument handling; calling configuration handler

--- modelTester/modelTester.py
+++ modelTester/modelTester.py
@@ -2,18 +2,38 @@
 # python modelTester.py --cfg=../test/configs/first_cfg.json
 
 import argparse
-from configurationHandler.cfgParser import ConfigParser
-from downloader import controller
+from configurationHandler.cfgParser import ConfigParser, cfgDesc
+# from downloader.controller import DownloaderController
 
 argParser = argparse.ArgumentParser()
-argParser.add_argument("--cfg", type=str, required=True, action="store", dest="path_cfg", help="Path to config")
+argParser.add_argument("--cfg", type=str, action="store", dest="path_cfg", help="Path to config")
+argParser.add_argument("--cfg-help", default=False, action="store_true", help="Prints configuration file description.")
+
+argParser.usage = f'<pyhon> modelTester.py --cfg=<PATH_CFG>'
+argParser.description = "a"
 
 if __name__ == "__main__":
     args = argParser.parse_args()
 
+    # Print configuration file description:
+    if args.cfg_help:
+        print(cfgDesc)
+        exit(0)
+    # Required argument for application run:
+    if not args.path_cfg:
+        print("the following arguments are required: --cfg")
+        exit(2)
+
+    # Configuration handler - parse cfg file:
     cfgParser = ConfigParser(args.path_cfg)
+    # Configuration for Downloader component:
     configurationDownloader = cfgParser.getDownloaderCfg()
+    # Configuration for Model Evaluator component:
     configurationModelEvaluator = cfgParser.getEvaluatorCfg()
 
-    # downloader = 
+    # # Initialize Downloader component:
+    # downloader = DownloaderController(configurationDownloader)
+    # m, d = downloader.download()
+    # print(m)
+    # print(d)
 
